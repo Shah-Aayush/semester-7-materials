a984254ba358079131050aa544809b4db60ba1c8
shah-aayush <shahaayush349@gmail.com>
2022-11-18 14:05:24 +0530

    add materials

--- Big Data Analytics/Lab/Practical 4/average/WCDriver.java
+++ Big Data Analytics/Lab/Practical 4/average/WCDriver.java
@@ -1,14 +1,26 @@
+//import java.io.IOException;
+//import org.apache.hadoop.fs.Path;
+//import org.apache.hadoop.io.IntWritable;
+//import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.mapred.FileInputFormat;
+//import org.apache.hadoop.mapred.FileOutputFormat;
+//import org.apache.hadoop.mapred.JobClient;
+//import org.apache.hadoop.mapred.JobConf;
+//import org.apache.hadoop.util.Tool;
+//import org.apache.hadoop.util.ToolRunner;
+
 import java.io.IOException;
-import org.apache.hadoop.conf.Configured;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class WCDriver {
     public static void main(String args[]) throws Exception {
         Configuration conf = new Configuration();
